e4772f9e85f60072d461490c60d4fe437b52a5ad
Alexandre Vannobel <alex.vannobel@gmail.com>
2018-04-11 20:20:48 -0400

    New learning rate with arch 3

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import torchvision.transforms as transforms
 from torchvision.utils import save_image
 from torch import nn
 from torch import optim
+from torch.utils.data.sampler import SubsetRandomSampler
 
 import numpy as np
 import random
@@ -29,17 +30,27 @@ batch_size = 100
 valid_batch_size = 100
 test_batch_size = 50
 
-le_ra = 0.0001
+le_ra = 0.00001
 nombre_epochs = 100
 
 oneall = (torch.ones(10000)).byte()
 sampler = (torch.round(torch.rand(10000))).byte()
 
+num_train = len(train_set)
+indices = list(range(num_train))
+split = int(np.floor(0.25 * num_train))
+
+np.random.shuffle(indices)
+
+train_idx, valid_idx = indices[split:], indices[:split]
+train_sampler = SubsetRandomSampler(train_idx)
+valid_sampler = SubsetRandomSampler(valid_idx)
+
 train_loader = torch.utils.data.DataLoader(train_set,
-    batch_size=batch_size, shuffle=False, sampler=sampler)
+    batch_size=batch_size, shuffle=False, sampler=train_sampler)
 
 valid_loader = torch.utils.data.DataLoader(valid_set,
-    batch_size=valid_batch_size, shuffle=False, sampler=torch.abs(oneall-sampler))
+    batch_size=valid_batch_size, shuffle=False, sampler=valid_sampler)
 
 test_loader = torch.utils.data.DataLoader(test_set,
     batch_size=test_batch_size, shuffle=True)
@@ -52,62 +63,103 @@ def to_img(x):
     return x
 
 class FcNetwork(nn.Module):
-    ch_dconv1 = 25
-    ch_dconv2 = 50
+    ch_dconv1 = 16
+    ch_dconv2 = 32
     #ch_dconv3 = 30
     #ch_uconv1 = 20
-    ch_uconv2 = 25
+    ch_uconv2 = 16
     ch_uconv3 = 1
 
     hidden_layer_size = 1000
     def __init__(self):
         super().__init__()
         # Arch 1
-        #self.dconv1 = nn.Conv2d(1, self.ch_dconv1, kernel_size=3, padding=1, stride=2)
-        #self.dconv2 = nn.Conv2d(self.ch_dconv1, self.ch_dconv2, kernel_size=3, padding=1, stride=2)
-        #self.drop = nn.Dropout2d()
-        #self.upsamp = nn.Upsample(scale_factor=2, mode='nearest')
-        #self.uconv1 = nn.ConvTranspose2d(self.ch_dconv2, self.ch_uconv2, kernel_size=3, padding=1)
-        #self.uconv2 = nn.ConvTranspose2d(self.ch_uconv2, self.ch_uconv3, kernel_size=3, padding=1)
+        self.dconv1 = nn.Conv2d(1, self.ch_dconv1, kernel_size=3, padding=1, stride=2)
+        self.dconv2 = nn.Conv2d(self.ch_dconv1, self.ch_dconv2, kernel_size=3, padding=1, stride=2)
+        self.drop = nn.Dropout2d()
+        self.upsamp = nn.Upsample(scale_factor=2, mode='nearest')
+        self.uconv1 = nn.ConvTranspose2d(self.ch_dconv2, self.ch_uconv2, kernel_size=3, padding=1)
+        self.uconv2 = nn.ConvTranspose2d(self.ch_uconv2, self.ch_uconv3, kernel_size=3, padding=1)
 
         # Arch 2
         self.d1 = nn.Conv2d(1, 8, kernel_size=3, padding=0, stride=1)
         self.d2 = nn.Conv2d(8, 16, kernel_size=3, padding=0, stride=1)
         self.d3 = nn.Conv2d(16, 32, kernel_size=3, padding=1, stride=2)
         self.d4 = nn.Conv2d(32, 32, kernel_size=3, padding=1, stride=2)
-        self.d5 = nn.Conv2d(32, 32, kernel_size=3, padding=1, stride=2)
 
         self.upsamp = nn.Upsample(scale_factor=2, mode='nearest')
-        self.u5 = nn.ConvTranspose2d(32, 32, kernel_size=3, padding=1)
         self.u4 = nn.ConvTranspose2d(32, 32, kernel_size=3, padding=1)
         self.u3 = nn.ConvTranspose2d(32, 16, kernel_size=3, padding=1)
         self.u2 = nn.ConvTranspose2d(16, 8, kernel_size=3, padding=0)
         self.u1 = nn.ConvTranspose2d(8, 1, kernel_size=3, padding=0)
 
+        # Arch 3 (ajouts dans le milieu de la couche)
+        self.d3point5 = nn.Conv2d(32, 32, kernel_size=3, padding=0)
+        self.u3point5 = nn.ConvTranspose2d(32, 32, kernel_size=3, padding=0)
+
+        # Arch 4
+        self.d4point5 = nn.Conv2d(32, 32, kernel_size=3, padding=0)
+        self.d5point5 = nn.Conv2d(32, 32, kernel_size=3, padding=0)
+        self.u4point5 = nn.ConvTranspose2d(32, 32, kernel_size=3, padding=0)
+        self.u5point5 = nn.ConvTranspose2d(32, 32, kernel_size=3, padding=0)
+
+
+
 #A mettre quand on herite __init__ __len__ __getItem__
     def forward(self, image):
-        # Arch 1
-        # Encodeur
-        #x = F.relu(self.dconv1(image))
-        #x = F.relu(self.dconv2(x))
+        # # Arch 1
+        # # Encodeur
+        # x = F.relu(self.dconv1(image))
+        # x = F.relu(self.dconv2(x))
+        #
+        # # Decodeur
+        # x = self.upsamp(x)
+        # x = F.relu(self.uconv1(x))
+        # x = self.upsamp(x)
+        # x = F.tanh(self.uconv2(x))
 
-        # Decodeur
-        #x = self.upsamp(x)
-        #x = F.relu(self.uconv1(x))
-        #x = self.upsamp(x)
-        #x = F.tanh(self.uconv2(x))
+        # # Arch 2
+        # # Encodeur
+        # x = F.relu(self.d1(image))
+        # x = F.relu(self.d2(x))
+        # x = F.relu(self.d3(x))
+        # x = F.relu(self.d4(x))
+        # # Décodeur
+        # x = F.relu(self.u4(self.upsamp(x)))
+        # x = F.relu(self.u3(self.upsamp(x)))
+        # x = F.relu(self.u2(x))
+        # x = F.tanh(self.u1(x))
 
-        # Arch 2
+        # Arch 3
         # Encodeur
         x = F.relu(self.d1(image))
         x = F.relu(self.d2(x))
         x = F.relu(self.d3(x))
+        x = F.relu(self.d3point5(x))
         x = F.relu(self.d4(x))
         # Décodeur
         x = F.relu(self.u4(self.upsamp(x)))
+        x = F.relu(self.u3point5(x))
         x = F.relu(self.u3(self.upsamp(x)))
         x = F.relu(self.u2(x))
         x = F.tanh(self.u1(x))
+
+        # # Arch 4
+        # # Encodeur
+        # x = F.relu(self.d1(image))
+        # x = F.relu(self.d2(x))
+        # x = F.relu(self.d3(x))
+        # x = F.relu(self.d3point5(x))
+        # x = F.relu(self.d4point5(x))
+        #
+        # # Décodeur
+        # x = F.relu(self.u4point5(x))
+        # x = F.relu(self.u3point5(x))
+        # x = F.relu(self.u3(self.upsamp(x)))
+        # x = F.relu(self.u2(x))
+        # x = F.tanh(self.u1(x))
+
+
         return x
 
 def train(model, train_loader, optimizer, epoch):
@@ -128,7 +180,7 @@ def train(model, train_loader, optimizer, epoch):
     save_image(outpic, './dc_img/NewimageOutput_{}.png'.format(epoch))
     return model, epoch_loss.data[0] * 100000
 
-def valid(model, valid_loader) :
+def valid(model, valid_loader, epoch) :
     model.eval()
     valid_loss = 0
     for data, target in valid_loader:
@@ -136,6 +188,10 @@ def valid(model, valid_loader) :
         output = model(data)
         valid_loss += nn.MSELoss()(output, data)
     valid_loss /= len(valid_loader.dataset)
+    pic = to_img(data.cpu().data)
+    save_image(pic, './dc_img/NewvalidimageInput_{}.png'.format(epoch))
+    outpic = to_img(output.cpu().data)
+    save_image(outpic, './dc_img/NewvalidimageOutput_{}.png'.format(epoch))
     return valid_loss.data[0] * 100000
 
 def test(model, test_loader, noise):
@@ -167,7 +223,7 @@ def experiment(model, epochs=nombre_epochs, lr=le_ra):
     for epoch in range(1, epochs + 1):
         model, epoch_loss = train(model, train_loader, optimizer, epoch)
         losses.append(epoch_loss)
-        valid_loss = valid(model, valid_loader)
+        valid_loss = valid(model, valid_loader, epoch)
         print("epoch [{}/{}], train loss = {} and valid loss = {}".format(epoch, nombre_epochs, epoch_loss, valid_loss))
         v_losses.append(valid_loss)
         if valid_loss < best_loss:
@@ -175,7 +231,7 @@ def experiment(model, epochs=nombre_epochs, lr=le_ra):
             best_model = model
     r = np.arange(0, epochs)
     plt.plot(r, losses, r, v_losses)
-    plt.title("Loss reseau autoencod conv 2 couches\n{} epochs et LR = {}".format(epochs, lr))
+    plt.title("Loss architecture 4\n{} epochs et LR = {}".format(epochs, lr))
     plt.xlabel("Epochs")
     plt.ylabel("Loss")
     plt.show()
